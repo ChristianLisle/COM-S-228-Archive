7e9934dd45ee90760e37ce632f57cb72dc6eb737
Christian Lisle <42817415+ChristianLisle@users.noreply.github.com>
2020-04-01 23:46:31 -0500

    Assignment 3
    This assignment dealt with using a doubly linked list, list iterator, and nodes to create lists of Prime Factors that represented numbers.

---
+++ A3_PrimeFactorization/src/hw3/PrimeFactor.java
@@ -0,0 +1,40 @@
+package edu.iastate.cs228.hw3;
+
+/**
+ * 
+ * @author Christian Lisle
+ *
+ * This class represents a Prime Factor object
+ * 
+ */
+public class PrimeFactor {
+	public int prime; // prime factor
+	public int multiplicity; // number of times the prime factor appears in a factorization
+
+	/**
+	 * Precondition: p is a prime number.
+	 * 
+	 * @param p prime
+	 * @param m multiplicity
+	 * @throws IllegalArgumentException if m < 1
+	 */
+	public PrimeFactor(int p, int m) throws IllegalArgumentException {
+		if (m < 1) throw new IllegalArgumentException();
+		prime = p;
+		multiplicity = m;
+	}
+
+	@Override
+	public PrimeFactor clone() {
+		return new PrimeFactor(prime, multiplicity);
+	}
+
+	/**
+	 * Prints out, for instance "2^3" if prime == 2 and multiplicity == 3, or "5" if
+	 * prime == 5 and multiplicity == 1.
+	 */
+	@Override
+	public String toString() {
+		return (multiplicity == 1) ? prime + "" : prime + "^" + multiplicity;
+	}
+}
